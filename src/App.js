1b96b87129740bc40bb8d4d2bcb9632e152c2996
Gertobin11 <gertobin11@gmail.com>
2022-12-19 18:32:38 +0000

    Add scroll progress at the top of screen with framer motion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import "./App.css";
 import MainNav from "./components/MainNav";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
   return (
     <>
       <MainNav />
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <About />
       <Projects />
     </>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,9 +8,11 @@ import styles from "../styles/Projects.module.css";
 const Projects = () => {
   const ref = useRef(null);
   const isInView = useInView(ref, { once: false });
+
   return (
     <section className={styles["project-section"]}>
-      <h3 ref={ref}
+      <h3
+        ref={ref}
         style={{
           opacity: isInView ? 1 : 0,
           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
