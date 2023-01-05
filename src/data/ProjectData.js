254e52894a9abf106e14be6cc04f2641380a0f90
Gertobin11 <gertobin11@gmail.com>
2023-01-05 13:02:12 +0000

    Add new images which are reduced in size for page performance

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,7 +4,7 @@ export const projectData = [
     title: "Album Discussion",
     link: "https://albumdiscussion.gertobin.org/",
     image:
-      "https://res.cloudinary.com/dyg53cwvw/image/upload/v1672922773/website%20images/albumdiscussion_qoydo1.webp",
+      "https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/albumdiscussion_1_wezvvx.webp",
     description:
       "A website for people to see other users album reviews, post their own reviews and create a wishlist of albums they like. Key features - Use of plotly to show user statistics - Use of wikipedia API to get info for users- Abilty to follow other users and see their reviews- Customizeable Profile",
   },
@@ -12,21 +12,21 @@ export const projectData = [
     id: 2,
     title: 'Paintball Kerry',
     link: 'https://www.paintballkerry.com/',
-    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672922773/website%20images/paintballkerry_gugubu.webp',
+    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/paintballkerry_1_shnrb7.webp',
     description: 'A website for a local business, designed to provide key informton to customers and provide a way of contacting them, Built on Django with Bootstrap. '
   },
   {
     id: 3,
     title: 'The Vinyl Hub',
     link: 'https://thevinylhub.gertobin.org/',
-    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672922773/website%20images/thevinylhub_di7s5u.webp',
+    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/thevinylhub_1_hfjrfo.webp',
     description: 'An eccomerce website with the purpose of selling classic and modern vinyls, Built with Django and Bootstrap. Stripe payment integration and AWS S3 implemented for payment and static / media files hosting'
   },
   {
     id: 4,
     title: 'Our Lockdown Reads',
     link: 'https://ourlockdownreads.gertobin.org/',
-    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672922773/website%20images/lockdownreads_xglero.webp',
+    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923558/website%20images/lockdownreads_1_odxaql.webp',
     description: 'A website to allow users to upload the books they have read through lockdown and review those books. Built with Flask and MongoDB'
   }
 ];
