1873915ce9ed4e1aa34d36da655d9c4476390a3f
Gertobin11 <gertobin11@gmail.com>
2023-01-05 12:21:00 +0000

    Add id to about section for navs to function correctly

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ const About = () => {
   const content =
     "I am a Full Stack Web Developer based in Tralee County Kerry, Ireland. I am adept at using HTML5, CSS, JavaScript and Python. I am proficient at using jQuery, Bootstrap, Flask and Django frameworks. I have experience with SQL and NoSQL databases.If you are an employer wishing to  hire me or another web developer looking to collaborateyou can simply go to my ";
   return (
-    <section className={styles.about}>
+    <section className={styles.about} id="about">
       <div className={styles["black-overlay"]}>
         <div className={styles["about-text"]}>
             <TextContainer
