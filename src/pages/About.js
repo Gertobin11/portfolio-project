a00bb3e10313feffa5624ffd0fddbf7312df5230
Gertobin11 <gertobin11@gmail.com>
2023-01-06 13:22:44 +0000

    Add aboutData to separate the about text to a separate file

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,27 +2,28 @@ import React from "react";
 import styles from "../styles/About.module.css";
 import TextContainer from "../components/TextContainer";
 import Cta from "../components/Cta";
+import { about } from "../data/AboutData";
 
 const About = () => {
   const title = "Ger Tobin | Web Developer";
-  const content =
-    "I am a Full Stack Web Developer based in Tralee County Kerry, Ireland. I am adept at using HTML5, CSS, JavaScript and Python. I am proficient at using jQuery, Bootstrap, Flask and Django frameworks. I have experience with SQL and NoSQL databases.If you are an employer wishing to  hire me or another web developer looking to collaborateyou can simply go to my ";
+  const content = about;
+
   return (
     <section className={styles.about} id="about">
       <div className={styles["black-overlay"]}>
         <div className={styles["about-text"]}>
-            <TextContainer
+          <TextContainer
             title={title}
             content={content}
             cta="#projects"
             children="My Projects"
-            />
+          />
         </div>
-        <div className={styles['skills-layout']}>
-          <Cta location='#skills' title='Check Out My Skills' />
+        <div className={styles["skills-layout"]}>
+          <Cta location="#skills" title="Check Out My Skills" />
         </div>
-        <div className={styles['contact-layout']}>
-          <Cta location='#contact' title='Contact Me Here' />
+        <div className={styles["contact-layout"]}>
+          <Cta location="#contact" title="Contact Me Here" />
         </div>
       </div>
     </section>
