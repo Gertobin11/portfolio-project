962975f7357711ce94b0a8b5798f59da11ff5ff8
Gertobin11 <gertobin11@gmail.com>
2026-02-02 15:39:43 +0000

    removed previous personal projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,46 +1,24 @@
 export const projectData = [
+
   {
     id: 1,
-    title: "Album Discussion",
-    link: "https://albumdiscussion.gertobin.dev/",
-    image:
-      "https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/albumdiscussion_1_wezvvx.webp",
-    description:
-      "A website for people to see other users album reviews, post their own reviews and create a wishlist of albums they like. Key features - Use of plotly to show user statistics - Use of wikipedia API to get info for users- Abilty to follow other users and see their reviews- Customizeable Profile",
-  },
-  {
-    id: 2,
     title: 'Paintball Kerry',
     link: 'https://www.paintballkerry.com/',
     image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/paintballkerry_1_shnrb7.webp',
     description: 'A website for a local business, designed to provide key information to customers and provide a way of contacting them, Built on Django with Bootstrap. '
   },
     {
-    id: 3,
+    id: 2,
     title: 'Guerin Photography',
     link: 'https://guerinphotography.ie',
     image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1770032743/Screenshot_from_2026-02-02_11-43-00_zgqhjh.webp',
-    description: 'A website for a local photographer. Implemented in Sveltekit, designed for spedd and visuals. Allows for image uploads and portfolio creation for the user with auto image optimisation using Sharp.'
+    description: 'A website for a local photographer. Implemented in Sveltekit, designed for speed and visuals. Allows for image uploads and portfolio creation for the user with auto image optimisation using Sharp.'
   },
     {
-    id: 4,
+    id: 3,
     title: 'Tralee Orthotics',
     link: 'https://traleeorthotics.ie',
     image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1770032743/Screenshot_from_2026-02-02_11-41-58_rg44z6.webp',
     description: 'A website for a local orthotics company. Built in Sveltekit with Sanity.io as a CMS for managing the inventory.'
-  },
-  {
-    id: 5,
-    title: 'The Vinyl Hub',
-    link: 'https://thevinylhub.gertobin.dev/',
-    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923559/website%20images/thevinylhub_1_hfjrfo.webp',
-    description: 'An eccomerce website with the purpose of selling classic and modern vinyls, Built with Django and Bootstrap. Stripe payment integration and AWS S3 implemented for payment and static / media files hosting'
-  },
-  {
-    id: 6,
-    title: 'Our Lockdown Reads',
-    link: 'https://ourlockdownreads.gertobin.dev/',
-    image: 'https://res.cloudinary.com/dyg53cwvw/image/upload/v1672923558/website%20images/lockdownreads_1_odxaql.webp',
-    description: 'A website to allow users to upload the books they have read through lockdown and review those books. Built with Flask and MongoDB'
   }
 ];
